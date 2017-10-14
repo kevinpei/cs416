e7f9540c50c47c259609da6aae26f851b75e8b60
kevinpei <kevinsongpei@gmail.com>
2017-10-14 13:19:37 -0400

    Updated benchmark

--- asst1/code/Benchmark/externalCal.c
+++ asst1/code/Benchmark/externalCal.c
@@ -10,7 +10,6 @@
 #include "../my_pthread_t.h"
 
 #define THREAD_NUM 10
-#define VECTOR_SIZE 100000
 #define RAM_SIZE 160
 #define RECORD_NUM 10
 #define RECORD_SIZE 1024
@@ -24,16 +23,17 @@ int *mem = NULL;
 int sum = 0;
 
 void external_calculate(void* arg) {
+	int i = 0, j = 0;
 	char *t_name = (char *) arg;
 	int n = atoi(t_name) - 1;
 	int itr = RECORD_SIZE / (RAM_SIZE / THREAD_NUM);
 
 	int fd = open(strcat("./record/", t_name), O_RDONLY);
 
-	for (int i = 0; i < itr; ++i) {
+	for (i = 0; i < itr; ++i) {
 		// read 16B from nth record into mem[n]
 		read(fd, mem + n*16, 16);
-		for (int j = 0; j < 40; ++j) {
+		for (j = 0; j < 40; ++j) {
 			my_pthread_mutex_lock(&mutex);
 			sum += mem[j];
 			my_pthread_mutex_unlock(&mutex);
@@ -44,17 +44,17 @@ void external_calculate(void* arg) {
 
 
 int main() {
-
+	int i = 0;
 	char name[2];
 
 	mem = (int*)malloc(RAM_SIZE);
 
-	for (int i = 0; i < THREAD_NUM; ++i) {
+	for (i = 0; i < THREAD_NUM; ++i) {
 		sprintf(name, "%d", i+1);
 		my_pthread_create(&thread[i], NULL, &external_calculate, name);
 	}
 
-	for (int i = 0; i < THREAD_NUM; ++i)
+	for (i = 0; i < THREAD_NUM; ++i)
 		my_pthread_join(thread[i], NULL);
 
 	return 0;
